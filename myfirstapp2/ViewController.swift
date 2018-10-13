eaa879dfa6c5917decee8c09e6ce7957a34f2c81
chang <super57033@gmail.com>
2018-10-13 20:55:26 +0800

    newApp4Mashi

--- myfirstapp2/ViewController.swift
+++ myfirstapp2/ViewController.swift
@@ -7,12 +7,78 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func bt1(_ sender: Any) {
+        if let path = Bundle.main.path(forResource: "mashi_makingCake", ofType: "mp4"){
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true, completion:{
+                video.play()
+                
+            })
+        }
+    }
+    
+    @IBAction func bt2(_ sender: Any) {
+        if let path = Bundle.main.path(forResource: "mashi_fishing", ofType: "mp4"){
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true, completion:{
+                video.play()
+                
+            })
+        }
+    }
+    
+    @IBAction func bt3(_ sender: Any) {
+        if let path = Bundle.main.path(forResource: "mashi_watchingMoon", ofType: "mp4"){
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true, completion:{
+                video.play()
+                
+            })
+        }
+    }
+    
+    @IBAction func bt4(_ sender: Any) {
+        if let path = Bundle.main.path(forResource: "mashi_findingHouse", ofType: "mp4"){
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true, completion:{
+                video.play()
+                
+            })
+        }
+    }
+    
+    @IBAction func bt5(_ sender: Any) {
+        if let path = Bundle.main.path(forResource: "mashi_eatingEgg", ofType: "mp4"){
+            let video = AVPlayer(url: URL(fileURLWithPath: path))
+            let videoPlayer = AVPlayerViewController()
+            videoPlayer.player = video
+            
+            present(videoPlayer, animated: true, completion:{
+                video.play()
+                
+            })
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
